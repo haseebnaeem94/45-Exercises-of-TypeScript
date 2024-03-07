2d2462e241d84a86cb9c32039b62588612686787
haseebnaeem94 <162363763+haseebnaeem94@users.noreply.github.com>
2024-03-07 14:26:38 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-// Hello world
-console.log("Hello World");
+// Task # 2
+var personName = "ERIC";
+var message = "would you like to learn some python today";
+console.log("Hello ".concat(personName, ", \" ").concat(message, "\""));
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-// Hello world
+// Task # 2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
-console.log("Hello World");
+let personName: string = "ERIC";
+let message: string = "would you like to learn some python today";
+console.log(`Hello ${personName}, " ${message}"`);
